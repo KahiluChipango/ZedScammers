e5ae90c2fda589805c00b763b6cdfb1b84b3b8e4
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2023-12-11 21:11:55 +0200

    comment

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scam.Pezapo.com - Under Construction</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Additional styles or external CSS files can be linked here -->
+</head>
+<body class="font-sans bg-gray-100">
+
+<!-- Header -->
+<header class="bg-blue-500 text-white py-4">
+    <div class="container mx-auto">
+        <h1 class="text-2xl font-bold sm:text-3xl">Scam.Pezapo.com</h1>
+        <p class="text-sm sm:text-base">Exciting News! Introducing a new initiative in the fight against scammers.</p>
+    </div>
+</header>
+
+<!-- Main Content Area (Under Construction Message) -->
+<main class="container mx-auto mt-8 text-center">
+    <h2 class="text-3xl font-bold text-gray-800">Under Construction</h2>
+    <p class="text-lg text-gray-600">I've set up this platform to empower you. The website is currently in development, so stay tuned for updates!</p>
+    <p class="mt-4"><a href="https://www.facebook.com/JUSTaGUYonFB?mibextid=ZbWKwL" target="_blank" class="text-orange-500 underline hover:text-orange-600">Follow us on Facebook for more updates</a></p>
+</main>
+
+<!-- Footer -->
+<footer class="bg-gray-200 py-4">
+    <div class="container mx-auto text-center text-gray-600">
+        <p>&copy; 2023 Scam.Pezapo.com. All rights reserved.</p>
+    </div>
+</footer>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/admin', function () {
     return view('welcome');
 });
 
