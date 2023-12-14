781effaf55477a067c5788adabba19dad4748271
Kahilu Chipango <kahiluchipango@yahoo.com>
2023-12-14 15:12:49 +0200

    Updated Home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,33 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pezapo Scammers - Under Construction</title>
+    <title>Zed Scammers - Under Construction</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <!-- Additional styles or external CSS files can be linked here -->
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-
-        main {
-            flex-grow: 1;
-        }
-    </style>
 </head>
-<body class="font-sans bg-gray-100">
+<body class="font-sans bg-gray-100 flex flex-col min-h-screen">
 
 <!-- Header -->
 <header class="bg-blue-500 text-white py-4">
     <div class="container mx-auto flex items-center justify-between">
         <!-- Logo container -->
         <div class="flex items-center">
-            <img src="{{ asset('storage/Pics/Pezapo Scamers.png') }}" alt="Pezapo Scammers Logo" class="max-w-full h-12 mr-2">
-            <h1 class="text-2xl font-bold sm:text-3xl">Pezapo Scammers</h1>
+            <img src="{{ asset('storage/Pics/Pezapo Scamers.png') }}" alt="Zed Scammers Logo" class="max-w-full h-12 mr-2">
+            <h1 class="text-2xl font-bold sm:text-3xl">Zed Scammers</h1>
         </div>
 
-       {{-- <!-- Navigation -->
+        {{-- <!-- Navigation -->
         <nav class="flex items-center">
             <a href="#" class="text-white ml-4">Home</a>
             <a href="#" class="text-white ml-4">About</a>
@@ -41,16 +29,16 @@
 </header>
 
 <!-- Main Content Area (Under Construction Message) -->
-<main class="container mx-auto mt-8 text-center">
+<main class="container mx-auto mt-8 text-center flex-grow">
     <h2 class="text-3xl font-bold text-gray-800">Under Construction</h2>
     <p class="text-lg text-gray-600">I've set up this platform to empower you. The website is currently in development, so stay tuned for updates!</p>
-    <p class="mt-4"><a href="https://www.facebook.com/JUSTaGUYonFB?mibextid=ZbWKwL" target="_blank" class="text-orange-500 underline hover:text-orange-600">Follow us on Facebook for more updates</a></p>
+    <p class="mt-4"><a href="https://www.facebook.com/profile.php?id=61552005604710&mibextid=hIlR13" target="_blank" class="text-orange-500 underline hover:text-orange-600">Follow us on Facebook for more updates</a></p>
 </main>
 
 <!-- Footer -->
 <footer class="bg-gray-200 py-4">
     <div class="container mx-auto text-center text-gray-600">
-        <p>&copy; 2023 Pezapo Scammers. All rights reserved.</p>
+        <p>&copy; 2023 Zed Scammers. All rights reserved.</p>
     </div>
 </footer>
 
